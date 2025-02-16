4b28131de51615fc3a44f33a7a08762112995655
Bernardo Souza <39275805+BernardoS@users.noreply.github.com>
2025-02-16 02:41:07 +0000

    Deixa o `database.js` mais robusto ao lidar com erros e cria funções auxiliares

--- infra/database.js
+++ infra/database.js
@@ -9,11 +9,39 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
+}
+
+async function getCurrentVersion() {
+  const currentVersion = await query("SHOW server_version;");
+  return currentVersion.rows[0].server_version;
+}
+
+async function getMaxConnections() {
+  const currentVersion = await query("SHOW max_connections;");
+  return currentVersion.rows[0].max_connections;
+}
+
+async function getOpenConnections(databaseName) {
+  const openConnections = await query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  //,
+  return openConnections.rows[0].count;
 }
 
 export default {
   query: query,
+  getCurrentVersion: getCurrentVersion,
+  getMaxConnections: getMaxConnections,
+  getOpenConnections: getOpenConnections,
 };
