d8653a3a4da8dda6fe5ae2da5e8ba9f96df3ce69
Bernardo Souza <39275805+BernardoS@users.noreply.github.com>
2025-02-19 23:20:46 +0000

    Ajuste para o site lidar com certificados ssl auto assinados

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV == "development" ? false : true,
-  });
-
-  console.log("Credênciais do postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -55,3 +47,12 @@ export default {
   getMaxConnections: getMaxConnections,
   getOpenConnections: getOpenConnections,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV == "development" ? false : true;
+}
