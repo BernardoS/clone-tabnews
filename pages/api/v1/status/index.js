fcdb7b237fcaa99f41a3c0203aff3d5689cee546
Bernardo Souza <39275805+BernardoS@users.noreply.github.com>
2025-10-10 11:18:51 +0000

    feat: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,24 +1,36 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  const currentVersion = await database.getCurrentVersion();
-  const maxConnections = await database.getMaxConnections();
-  const getOpenConnections = await database.getOpenConnections(
-    process.env.POSTGRES_DB,
-  );
+    const currentVersion = await database.getCurrentVersion();
+    const maxConnections = await database.getMaxConnections();
+    const getOpenConnections = await database.getOpenConnections(
+      process.env.POSTGRES_DB,
+    );
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: currentVersion,
-        max_connections: parseInt(maxConnections),
-        opened_connections: getOpenConnections,
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: currentVersion,
+          max_connections: parseInt(maxConnections),
+          opened_connections: getOpenConnections,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\nErro dentro deo catch do controller");
+    console.log(publicErrorObject);
+
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
