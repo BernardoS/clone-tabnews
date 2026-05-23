75892373d5ff648b8581752a5ba21ce3aa2116ec
Bernardo Souza <39275805+BernardoS@users.noreply.github.com>
2026-05-23 14:30:43 +0000

    style: fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,36 +8,93 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "coverage/**",
+      "dist/**",
+      "package-lock.json",
+    ],
   },
-  pluginReact.configs.flat.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+
+    plugins: {
+      js,
+      react: pluginReact,
+    },
+
+    extends: ["js/recommended"],
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": "off",
+      "react/jsx-uses-react": "off",
+      "react/jsx-uses-vars": "error",
+    },
+  },
+
+  {
+    files: ["tests/**/*.js"],
+
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+
   {
     files: ["**/*.json"],
     plugins: { json },
     language: "json/json",
     extends: ["json/recommended"],
   },
+
   {
     files: ["**/*.jsonc"],
     plugins: { json },
     language: "json/jsonc",
     extends: ["json/recommended"],
   },
+
   {
     files: ["**/*.json5"],
     plugins: { json },
     language: "json/json5",
     extends: ["json/recommended"],
   },
+
   {
     files: ["**/*.md"],
     plugins: { markdown },
     language: "markdown/gfm",
     extends: ["markdown/recommended"],
   },
+
   {
     files: ["**/*.css"],
     plugins: { css },
